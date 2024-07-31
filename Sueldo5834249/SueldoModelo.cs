9168375880353ff196fb1b6342080ac0a9f474f0
emelyyy03 <emelyyy03@outlook.com>
2024-07-31 16:09:47 -0600

    Agregar archivos de proyecto.

---
+++ Sueldo5834249/SueldoModelo.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sueldo5834249
+{
+    public partial class SueldoModelo:ObservableObject
+    {
+        [ObservableProperty]
+        public string? sueldo;
+
+        [ObservableProperty]
+        public double aumento;
+
+        [RelayCommand]
+        private void ActualizarDatos()
+        {
+            if(double.TryParse(Sueldo, out double sueldo))
+            {
+                if(sueldo <= 1000)
+                {
+                    Aumento = sueldo * 1.15;
+                }
+                else if(sueldo > 1000)
+                {
+                    Aumento = sueldo * 1.10;
+                }
+            }
+            else
+            {
+                Aumento.ToString("Digite valores numericos");
+            }
+        }
+
+    }
+}
